a71da6949198082a67068272b6b84c662976cd4b
superdeveloper910 <diego.romero89910@gmail.com>
2018-09-12 05:35:09 +0200

    layout

--- database/seeds/LayoutSeeder.php
+++ database/seeds/LayoutSeeder.php
@@ -15,7 +15,7 @@ class LayoutGenerate extends Seeder
          DB::table('layout')->insert([
 			    [		'id' 			=> '1',
 			    		'logo' 		=> '1526052756.png',
-			    		'site_name' 		=> 'Nycservices.sarapis.org',
+			    		'site_name' 		=> 'orservices.sarapis.org',
 			    		'tagline' 	=> 'This website provides information about health and human services available to New Yorkers.',
 			    		'contact_text' 			=> '',
 			    		'contact_btn_label' 		=> '',
