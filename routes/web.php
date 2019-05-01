3e837e6f4334a0354d82016e5902e3c0d1d421ac
Milos <grantlatest@gmail.com>
2019-05-01 22:13:54 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,7 @@ Route::get('/category_{id}', 'ServiceController@taxonomy');
 
 Route::get('/services_near_me', 'ExploreController@geolocation');
 
+Route::post('/filter', 'ExploreController@filter');
 Route::get('/filter', 'ExploreController@filter');
 
 // Route::post('/explore', 'ExploreController@index');
