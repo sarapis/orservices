c68ecb3bc9c31c74ea16a46f23554ddfd9ef8cb7
loveisbluenova <michalstroik@yandex.com>
2019-11-25 21:13:01 -0500

    fresh commit

--- public/frontend/assets/examples/js/forms/general.js
+++
@@ -1,8 +0,0 @@
-/*!
- * remark (http://getbootstrapadmin.com/remark)
- * Copyright 2017 amazingsurge
- * Licensed under the Themeforest Standard Licenses
- */
-jQuery(document).ready(function() {
-  Form.init();
-});
--- public/frontend/global/gulp/options/csslint.js
+++
@@ -1,7 +0,0 @@
-// config
-var config = require('../../config.json');
-
-
-module.exports = {
-  csslintrc: config.source.sass + '/.csslintrc',
-};
