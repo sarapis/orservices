7a5c4db101d8439904be6fc4a4c52d1dc0f9c983
Benjamin <bmunyoki@heptanalytics.com>
2022-03-17 12:00:38 +0300

    Bug fixes - secure pagination links

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if (!\App::environment('local')) {
+            $this->app['request']->server->set('HTTPS','on');
+        }
     }
 }
